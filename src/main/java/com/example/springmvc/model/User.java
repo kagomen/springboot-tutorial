98f3d568b99870b210ea06e2373ed9f7590bf263
kagomen <fu.taz.1102@gmail.com>
2025-05-22 11:46:07 +0900

    Lombokの追加とUserクラスの修正

--- src/main/java/com/example/springmvc/model/User.java
+++ src/main/java/com/example/springmvc/model/User.java
@@ -5,7 +5,11 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor // 引数なしコンストラクタを生成
 public class User {
 
   @NotBlank private String name;
@@ -20,51 +24,9 @@ public class User {
 
   private List<String> interests;
 
-  public User() {}
-
   public User(String name, String email, int age) {
     this.name = name;
     this.email = email;
     this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public String getGender() {
-    return gender;
-  }
-
-  public List<String> getInterests() {
-    return interests;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
-
-  public void setInterests(List<String> interests) {
-    this.interests = interests;
   }
 }
