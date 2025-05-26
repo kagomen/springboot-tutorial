f3af0dbacc4ea7b730c3e341b73b20b4c35f5af7
kagomen <fu.taz.1102@gmail.com>
2025-05-26 16:22:27 +0900

    MapStructの導入

--- src/main/java/com/example/three_layer/mapper/TaskItem3Mapper.java
+++ src/main/java/com/example/three_layer/mapper/TaskItem3Mapper.java
@@ -4,33 +4,17 @@ import com.example.three_layer.dto.TaskItem3Request;
 import com.example.three_layer.dto.TaskItem3Response;
 import com.example.three_layer.dto.TaskItem3UpdateRequest;
 import com.example.three_layer.entity.TaskItem3;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component // Springがクラスをインスタンス化して保持してくれる
-public class TaskItem3Mapper {
+// MapStructが実装クラスを生成してくれる + @Component（Springがクラスをインスタンス化して保持してくれる）
+@Mapper(componentModel = "spring")
+public interface TaskItem3Mapper {
   // DTOからEntityに変換(対DB用)
-  public TaskItem3 toEntity(TaskItem3Request req) {
-    var entity = new TaskItem3();
-    entity.setTitle(req.getTitle());
-    entity.setDone(req.isDone());
-    return entity;
-  }
+  TaskItem3 toEntity(TaskItem3Request req);
 
   // DTOからEntityに変換(対DB用)
-  public TaskItem3 toEntity(Integer id, TaskItem3UpdateRequest req) {
-    var entity = new TaskItem3();
-    entity.setId(id);
-    entity.setTitle(req.getTitle());
-    entity.setDone(req.isDone());
-    return entity;
-  }
+  TaskItem3 toEntity(Integer id, TaskItem3UpdateRequest req);
 
   // EntityからDTOに変換（対クライアント用）
-  public TaskItem3Response toDTO(TaskItem3 entity) {
-    var dto = new TaskItem3Response();
-    dto.setId(entity.getId());
-    dto.setTitle(entity.getTitle());
-    dto.setDone(entity.isDone());
-    return dto;
-  }
+  TaskItem3Response toDTO(TaskItem3 entity);
 }
