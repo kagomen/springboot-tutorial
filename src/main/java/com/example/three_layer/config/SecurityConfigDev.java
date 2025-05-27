bf217d10922005b3401e80e369174b01385ec960
kagomen <fu.taz.1102@gmail.com>
2025-05-27 14:59:08 +0900

    プロファイル分離

--- src/main/java/com/example/three_layer/config/SecurityConfigDev.java
+++ src/main/java/com/example/three_layer/config/SecurityConfigDev.java
@@ -2,12 +2,14 @@ package com.example.three_layer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("dev")
 @Configuration
-public class SecurityConfig {
+public class SecurityConfigDev {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
